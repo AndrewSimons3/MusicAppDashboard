0a7cffb89a83c51b02a83c728b481a04124d9c22
AndrewSimons3 <simdrew07@gmail.com>
2021-02-15 20:30:19 -0600

    fixed material ui

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextFIeld'
 import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
 
 class Login extends React.Component {
   constructor(props) {
@@ -23,30 +27,57 @@ class Login extends React.Component {
   }
 
   render() {
-    console.log(this.props)
-  return (
-    <div>
-      <Grid container item xs={12} spacing={1}
-        direction="center"
-        justify="center"
-        alignItems="center">
-        <form>
-          <div>
-          <TextField id="standard-basic" label="First Name *" />
-          </div>
-          <div>
-          <TextField id="standard-basic" label="Last Name *" />
-          </div>
-          <div>
-          <Button onClick={this.props.loginFunc} variant="contained" color="primary">
-            Login
-          </Button>
-          </div>
-        </form>
+    return (
+      <form>
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+      <FormControlLabel
+        control={<Checkbox value="remember" color="primary"></Checkbox>}
+        label="Remember me"
+      />
+      <Button onClick={this.props.loginFunc}
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+      >
+        Sign In
+      </Button>
+      <Grid container>
+        <Grid item xs>
+          <Link href="#" variant="body2">
+            Forgot password?
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="#" variant="body2">
+            {"Don't have an account? Sign Up"}
+          </Link>
+        </Grid>
       </Grid>
-    </div>
-  )
-}
+    </form>
+    )
+  }
 }
 
-export default Login
+export default Login;
